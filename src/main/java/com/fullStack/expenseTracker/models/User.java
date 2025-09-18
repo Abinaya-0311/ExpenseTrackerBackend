beca56fcd4fc1df8d968c5375952e8ecb9f7bbab
Baranikumar954 <baranikumar952004@gmail.com>
2025-09-18 21:56:03 +0530

    try01

--- src/main/java/com/fullStack/expenseTracker/models/User.java
+++ src/main/java/com/fullStack/expenseTracker/models/User.java
@@ -1,30 +1,27 @@
 package com.fullStack.expenseTracker.models;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Builder
 @Entity
 @Table(name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
         })
-@Builder
-@Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,31 +32,29 @@ public class User {
 
     @NotBlank
     @Email
+    @Column(unique = true, nullable = false)
     private String email;
 
     @NotBlank
-    private String password;
+    private String password; // Hash this before saving
 
-    @Column(name = "verification_code")
     private String verificationCode;
 
-    @Column(name = "verification_code_expiry_time")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date verificationCodeExpiryTime;
+    private LocalDateTime verificationCodeExpiryTime;
 
     @Column(nullable = false)
-    private boolean enabled;
+    private boolean enabled = false;
 
     private String profileImgUrl;
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(  name = "user_roles",
+    @JoinTable( name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
-
-    public User(String username, String email, String password, String verificationCode, Date verificationCodeExpiryTime, boolean enabled, Set<Role> roles) {
+    public User(String username, String email, String password, String verificationCode,
+                LocalDateTime verificationCodeExpiryTime, boolean enabled, Set<Role> roles) {
         this.username = username;
         this.email = email;
         this.password = password;
@@ -68,5 +63,4 @@ public class User {
         this.enabled = enabled;
         this.roles = roles;
     }
-
 }
